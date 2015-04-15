27f9362f9d334f1e710974ab83a789cece7165de
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2015-04-14 20:00:26 -0400

    adding files

--- workspace/Q39/Q39/Date.cpp
+++ workspace/Q39/Q39/Date.cpp
@@ -7,3 +7,60 @@
 //
 
 #include "Date.h"
+
+Date::Date(int month, int day, int year){
+    (*this).month = month;
+    (*this).day = day;
+    (*this).year = year;
+}
+
+void Date::setDate(int month, int day, int year){
+    (*this).month = month;
+    (*this).day = day;
+    (*this).year = year;
+}
+
+void Date::setMonth(int month){
+    (*this).month = month;
+}
+
+void Date::setDay(int day){
+    (*this).day = day;
+}
+
+void Date::setYear(int year){
+    (*this).year = year;
+}
+
+std::string Date::getDate(){
+    std::string s = "";
+    
+    s += (std::to_string(month) + "/" + std::to_string(day) + "/" + std::to_string(year));
+    
+    return s;
+}
+
+int Date::getMonth()const{
+    return month;
+}
+
+int Date::getDay()const{
+    return day;
+}
+
+int Date::getYear()const{
+    return year;
+}
+
+void Date::printNumber () const{
+    std::cout << month << "/" << day << "/" << year << std::endl;
+}
+
+void Date::printLetter() const{
+    static const std::string months[] = {"January", "February", "March", "April",
+        "May", "June", "July", "August", "September", "October", "Noveber",
+        "December"};
+    
+    std::cout << months[month] << " " << day << ", " << year << std::endl;
+}
+
--- workspace/Q39/Q39/Date.h
+++ workspace/Q39/Q39/Date.h
@@ -9,6 +9,26 @@
 #ifndef __Date__h
 #define __Date__h
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+class Date{
+private:
+    int month;
+    int day;
+    int year;
+public:
+    Date(int month, int day, int year);
+    void setDate(int month, int day, int year);
+    void setMonth(int month);
+    void setDay(int day);
+    void setYear(int year);
+    std::string getDate();
+    int getMonth()const;
+    int getDay()const;
+    int getYear()const;
+    void printNumber() const;
+    void printLetter() const;
+};
 
 #endif /* defined(__Date__h) */
